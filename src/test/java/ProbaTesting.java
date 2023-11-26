607c420bdcb61468dda8516c92551e5c8f4fd6c2
Joupiter <51340164+Joupiter@users.noreply.github.com>
2023-11-26 04:03:34 +0100

    prob

--- src/test/java/ProbaTesting.java
+++ src/test/java/ProbaTesting.java
@@ -25,7 +25,7 @@ public class ProbaTesting {
         System.out.printf("Total: %d \n", counter.values().stream().mapToInt(Integer::intValue).sum());
 
         System.out.println("-----------------------------");
-        probabilities.getItems().forEach((item, prob) -> System.out.printf("Item: %s | Prob: %f \n", item, prob));
+        probabilities.getItems().forEach((item, prob) -> System.out.printf("Item: %s | Prob: %d%% \n", item, prob.intValue()));
 
         System.out.println("-----------------------------");
         new Probabilities<>().add("pile", 50f).add("face", 50f).randomize().ifPresent(result -> System.out.printf("[CoinFlip] Result: %s \n", result));
@@ -37,76 +37,76 @@ public class ProbaTesting {
 
     private void execute(String item) {
         counter.merge(item, 1, Integer::sum);
-        System.out.printf("%s (x%d)\n", item, counter.get(item));
+        System.out.printf("%s (x%d) \n", item, counter.get(item));
     }
 
 }
 /*
 -----------------------------
 Common (x1)
-Epic (x1)
-Rare (x1)
 Common (x2)
 Common (x3)
 Common (x4)
-Epic (x2)
 Common (x5)
 Common (x6)
-Rare (x2)
-Epic (x3)
 Common (x7)
-Epic (x4)
-Rare (x3)
-Rare (x4)
-Rare (x5)
 Common (x8)
-Rare (x6)
 Common (x9)
 Common (x10)
 Common (x11)
 Common (x12)
-Rare (x7)
 Common (x13)
-Legendary (x1)
 Common (x14)
 Common (x15)
+Rare (x1)
+Epic (x1)
+Rare (x2)
+Rare (x3)
 Common (x16)
-Rare (x8)
 Common (x17)
 Common (x18)
-Rare (x9)
-Rare (x10)
 Common (x19)
 Common (x20)
 Common (x21)
-Epic (x5)
-Rare (x11)
 Common (x22)
+Rare (x4)
+Rare (x5)
+Rare (x6)
 Common (x23)
+Rare (x7)
+Rare (x8)
 Common (x24)
-Rare (x12)
 Common (x25)
-Rare (x13)
 Common (x26)
-Epic (x6)
 Common (x27)
-Epic (x7)
 Common (x28)
-Rare (x14)
+Common (x29)
+Epic (x2)
+Common (x30)
+Common (x31)
+Rare (x9)
+Common (x32)
+Common (x33)
+Rare (x10)
+Common (x34)
+Legendary (x1)
+Rare (x11)
+Rare (x12)
+Epic (x3)
 -----------------------------
 Item: Legendary | Occurrences: 1
-Item: Epic | Occurrences: 7
-Item: Rare | Occurrences: 14
-Item: Common | Occurrences: 28
+Item: Epic | Occurrences: 3
+Item: Rare | Occurrences: 12
+Item: Common | Occurrences: 34
 Total: 50
 -----------------------------
-Item: Epic | Prob: 10.000000
-Item: Rare | Prob: 30.000000
-Item: Legendary | Prob: 4.000000
-Item: Common | Prob: 83.000000
+Item: Rare | Prob: 30%
+Item: Legendary | Prob: 4%
+Item: Epic | Prob: 10%
+Item: Common | Prob: 83%
 -----------------------------
 [CoinFlip] Result: pile
 -----------------------------
-Operation took 17 ms
+Operation took 14 ms
 -----------------------------
 */
